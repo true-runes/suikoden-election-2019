c93ebddaf6d2031fae1bdc4906440c6dcafd7091
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-06-06 18:29:04 +0900

    Add unicorn:restart for starting unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,3 +4,10 @@ set :application, 'suikoden_election_2019'
 set :repo_url, 'https://github.com/corselia/suikoden-election-2019.git'
 set :rbenv_ruby, '2.6.3'
 set :linked_dirs, %w(log tmp/cache tmp/pids tmp/sockets bundle)
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
