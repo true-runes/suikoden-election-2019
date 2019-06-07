28b2c57c79e1d22a151da30d8af64a167ff6433c
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-06-07 11:35:20 +0900

    Revert "Fix that unicorn cannot reboot when deployed" because it seems not required
    This reverts commit 69d4efead00fe0d5d7422426796348587277a72e.

--- config/unicorn/development.rb
+++ config/unicorn/development.rb
@@ -7,9 +7,3 @@ preload_app true
 pid 'tmp/pids/unicorn.pid'
 stdout_path 'log/unicorn_stdout.log'
 stderr_path 'log/unicorn_stderr.log'
-
-# 再起動に失敗することを避ける（これしか方法はないのだろうか……）
-# https://github.com/corselia/suikoden-election-2019/issues/58
-before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = "#{ENV['APP_PATH']}/current/Gemfile"
-end
