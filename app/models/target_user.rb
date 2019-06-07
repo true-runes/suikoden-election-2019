166587187a0ef139b6a407a1c3ae821ed2a6ae6d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-06-08 08:36:18 +0900

    create-scope-for-counting-appropriate-vote

--- app/models/target_user.rb
+++ app/models/target_user.rb
@@ -4,4 +4,13 @@ class TargetUser < ApplicationRecord
   has_many :target_tweets
   has_many :deleted_tweets
   # rubocop:enable Rails/HasManyOrHasOneDependent
+
+  TWITTER_USER_ID_GENSOSENKYO = 1_471_724_029
+
+  # TODO: この first って挟むの非生産的なので、きっと何かスマートな方法がある
+  def self.gensosenkyo
+    TargetUser.
+      where(twitter_user_id: TWITTER_USER_ID_GENSOSENKYO)&.
+      first
+  end
 end
