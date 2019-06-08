350fae83ad15f301daf589da1267bcb614400815
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-06-08 18:06:21 +0900

    Implement each page title (shown dynamically)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # @page_name は各ビューの中で定義する
+  def page_title
+    if @page_name.nil?
+      "#{Constants::SITE_NAME}"
+    else
+      "#{Constants::SITE_NAME} - #{@page_name}"
+    end
+  end
 end
