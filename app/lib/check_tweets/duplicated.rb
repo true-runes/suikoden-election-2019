b56d8994a78d517ad0ce6890fb4e57d91c8cbef1
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-06-11 20:05:00 +0900

    Fix hash logic bugs

--- app/lib/check_tweets/duplicated.rb
+++ app/lib/check_tweets/duplicated.rb
@@ -2,16 +2,6 @@ class CheckTweets::Duplicated
   include MyTwitter::Client
 
   def duplicated_tweeted_twitter_user_id_str_and_its_count
-    # TODO: 以下の部分は汎用的に切り出すときっと便利（二つのモデルにまたがるので場所は要検討）
-    # valid_tweets_and_its_users = []
-    # TargetTweet.valid_vote.each do |tweet|
-    #   inserted_hash = {}
-    #   inserted_hash['twitter_user_id'] = tweet.target_user.twitter_user_id
-    #   inserted_hash['tweet_id'] = tweet.tweet_id
-
-    #   valid_tweets_and_its_users << inserted_hash
-    # end
-
     tweeted_twitter_user_ids = []
     TargetTweet.valid_vote.each do |tweet|
       tweeted_twitter_user_ids << tweet.target_user.twitter_user_id
@@ -24,33 +14,31 @@ class CheckTweets::Duplicated
 
     duplicated_tweeted_twitter_user_id_str_and_its_count = []
     shown_twitter_user_id_count.each do |twitter_user_id, shown_count|
-      if shown_count >= 2
-        inserted_hash = {}
+      next unless shown_count >= 2
 
-        inserted_hash['twitter_user_id_str'] = twitter_user_id.to_s
-        inserted_hash['count'] = shown_count
+      inserted_hash = {}
+      inserted_hash[twitter_user_id.to_s] = shown_count
 
-        duplicated_tweeted_twitter_user_id_str_and_its_count << inserted_hash
-      end
+      duplicated_tweeted_twitter_user_id_str_and_its_count << inserted_hash
     end
 
-    duplicated_tweeted_twitter_user_id_str_and_its_count
+    duplicated_tweeted_twitter_user_id_str_and_its_count # [{"585692587"=>3}, ...]
   end
 
   def duplicated_tweet_ids
     duplicated_tweet_ids = []
 
-    duplicated_tweeted_twitter_user_id_str_and_its_count.each do |twitter_user_id_str, _|
-      TargetUser.where(twitter_user_id: twitter_user_id_str.to_i).each do |target_user|
-        TargetTweet.where(target_tweet_id: target_user.id).each do |tweet|
-          duplicated_tweet_ids << tweet.tweet_id
-        end
+    duplicated_tweeted_twitter_user_id_str_and_its_count.each do |user_id_str_and_count_hash|
+      duplicated_tweeted_twitter_user_id = TargetUser.find_by(twitter_user_id: user_id_str_and_count_hash.keys.first.to_i)
+      TargetTweet.where(target_user_id: duplicated_tweeted_twitter_user_id).each do |tweet|
+        duplicated_tweet_ids << tweet.tweet_id
       end
     end
 
     duplicated_tweet_ids
   end
 
+  # TODO: バルクで取得する
   # tweet_id_and_can_see_tweet = [{ tweet_id_str: "123456", can_see_tweet: true }, ...]
   def tweet_id_and_can_see_tweet(tweet_ids_array)
     fetch_interval = 10
@@ -59,8 +47,14 @@ class CheckTweets::Duplicated
     tweet_ids_array.each do |tweet_id|
       inserted_hash = {}
 
-      inserted_hash['tweet_id_str']   = tweet_id.to_s
-      inserted_hash['can_see_tweet']  = @my_twitter_client.status(tweet_id).nil? ? false : true
+      begin
+        inserted_hash[tweet_id.to_s] = true if @my_twitter_client.status(tweet_id)
+      rescue Twitter::Error => e
+        # Twitter::Error::NotFound: No status found with that ID.
+        # Twitter::Error::Forbidden: Sorry, you are not authorized to see this status.
+        inserted_hash[tweet_id.to_s] = false
+        puts "#{e}: #{tweet_id}"
+      end
 
       tweet_id_and_can_see_tweet << inserted_hash
       sleep fetch_interval
@@ -69,4 +63,3 @@ class CheckTweets::Duplicated
     tweet_id_and_can_see_tweet
   end
 end
-# valid_tweets_and_its_users.each do |tweet_and_user_hash|
