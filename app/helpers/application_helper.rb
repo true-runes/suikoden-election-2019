8cb9c935547ac96d417248e6839b4d53d26fbefb
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-06-15 13:40:19 +0900

    [WIP]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,31 @@ module ApplicationHelper
   def formatted_time_description(time_class)
     time_class.strftime('%Y/%m/%d %H:%M:%S')
   end
+
+  def remove_t_co(text)
+    text.gsub(/https:\/\/t\.co\/[0-9a-zA-Z]+/, '')
+  end
+
+  # TODO: データベース設計が破滅しているのでここで吸収している
+  def media_uris(target_tweet_object)
+    media_uris = []
+
+    unless target_tweet_object.media_url_https_01.nil?
+      media_uris << target_tweet_object.media_url_https_01
+    end
+
+    unless target_tweet_object.media_url_https_01.nil?
+      media_uris << target_tweet_object.media_url_https_02
+    end
+
+    unless target_tweet_object.media_url_https_01.nil?
+      media_uris << target_tweet_object.media_url_https_03
+    end
+
+    unless target_tweet_object.media_url_https_01.nil?
+      media_uris << target_tweet_object.media_url_https_04
+    end
+
+    media_uris
+  end
 end
