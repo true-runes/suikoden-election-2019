2f0cdde9ba1241fd57d546687a195317d24649ee
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-06-15 23:01:43 +0900

    Implement a minimum recommended lines page

--- app/controllers/results/recommended_lines_controller.rb
+++ app/controllers/results/recommended_lines_controller.rb
@@ -1,5 +1,6 @@
 class Results::RecommendedLinesController < ApplicationController
   def index
-    @bar = 'foo'
+    recommended_lines_csv_file = Rails.root.join('db', 'recommended_lines.csv')
+    @recommended_lines_from_csv = CSV.read(recommended_lines_csv_file)
   end
 end
