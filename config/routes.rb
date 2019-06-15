8cef960ae9d4649612ba4d37c1238bb8406d4e8e
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-06-15 17:56:36 +0900

    Add results page's route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   resources :result_tweets, only: [:index]
+  resources :results, only: [:index]
 
   namespace :pages do
     match 'check_my_vote', to: 'check_my_vote#index', via: [:get, :post]
