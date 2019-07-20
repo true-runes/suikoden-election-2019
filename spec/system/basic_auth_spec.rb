c4f0d793e85b1a44afd31c83335a8f0767990410
corselia <takiya@toran.sakura.ne.jp>
2019-07-21 00:01:18 +0900

    Write two specs

---
+++ spec/system/basic_auth_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+# Only NOT production environments such as development and test
+def visit_with_http_auth(target_path)
+  username = ENV['BASIC_AUTH_USERNAME']
+  password = ENV['BASIC_AUTH_PASSWORD']
+  visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{target_path}"
+end
+
+RSpec.feature 'ベーシック認証', type: :system do
+  scenario '登録されているIDとパスワードで認証が成功すること' do
+    visit_with_http_auth root_path
+    expect(page).to have_content '幻水総選挙2019'
+  end
+
+  scenario '登録されていないIDとパスワードでは認証が失敗すること' do
+    username    = 'foo'
+    password    = 'bar'
+    target_path = root_path
+
+    visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{target_path}"
+
+    expect(page).to have_content 'HTTP Basic: Access denied.'
+  end
+end
