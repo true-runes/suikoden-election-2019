f525f9f58ed45128406e3b2480ed46a2d705aa7a
corselia <takiya@toran.sakura.ne.jp>
2019-07-20 23:55:09 +0900

    Set a RSpec environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
 
   def basic_auth
-    if Rails.env.development?
+    unless Rails.env.production?
       authenticate_or_request_with_http_basic do |user, password|
         user == ENV.fetch('BASIC_AUTH_USERNAME') && password == ENV.fetch('BASIC_AUTH_PASSWORD')
       end
